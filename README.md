4b5595219a9fe995d1bf911251bfb420fbeba16a
chenggong <chenggong@asiainfo.com>
2020-03-23 23:41:39 +0800

    20200323新增

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # gmail1203 本地修改
-#gmail-service-user用户服务service层  8070
-#gmail-web-user用户服务web层   8080
+gmail-user-service用户服务service层  8070
+gmail-user-web用户服务web层   8080
+
+gmail-manage-service用户服务service层  8071
+gmail-manage-web用户服务web层   8081
---
+++ gmail-manage-service/src/main/java/com/guli/gmail/manage/GmailManageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.guli.gmail.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmailManageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmailManageServiceApplication.class, args);
+    }
+
+}
